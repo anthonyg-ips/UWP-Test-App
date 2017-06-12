30efd465da728cb40ad35430a99ecf523f55b3a1
anthonyg-ips <anthonyg@ips-yes.com>
2017-06-12 15:23:52 -0400

    Initial Commit

---
+++ Samples/MagneticStripeReader/cs/SampleConfiguration.cs
@@ -0,0 +1,45 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.PointOfService;
+using Windows.UI.Xaml.Controls;
+using MagneticStripeReaderSample;
+namespace SDKTemplate
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Magnetic Stripe Reader";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title = "BankCardDataReceived Event", ClassType = typeof(Scenario1_BankCards) },
+            new Scenario() { Title = "AamvaCardDataReceived Event", ClassType = typeof(Scenario2_AamvaCards) }
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+
+    public partial class DeviceHelpers
+    {
+        public static async Task<MagneticStripeReader> GetFirstMagneticStripeReaderAsync()
+        {
+            return await DeviceHelpers.GetFirstDeviceAsync(MagneticStripeReader.GetDeviceSelector(), async (id) => await MagneticStripeReader.FromIdAsync(id));
+        }
+    }
+}
